86077f6f48f8f4a30592461291ca7c1246e8e9b4
wsx3672 <31624875+wsx3672@users.noreply.github.com>
2017-09-22 18:02:24 +0900

    20170922 상태패턴완료.

---
+++ LastClass/DrawingResize.h
@@ -0,0 +1,20 @@
+#ifndef _DRAWINGRESIZE_H
+#define _DRAWINGRESIZE_H
+#include "MouseLButtonAction.h"
+
+class DrawingResize :public MouseLButtonAction {
+public:
+	static MouseLButtonAction* Instance();
+
+	virtual void MouseLButtonUp(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY);
+	virtual void MouseLButtonDown(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY);
+	virtual void MouseLButtonDrag(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY, CPaintDC *cPatinDC);
+
+
+protected:
+	//DrawingClass();
+private:
+	static DrawingResize* instance;
+};
+
+#endif // !_DRAWINGRESIZE_H
---
+++ LastClass/MultipleSelectionState.cpp
@@ -0,0 +1,22 @@
+#include "MultipleSelectionState.h"
+
+
+MultipleSelectionState* MultipleSelectionState::instance = 0;
+
+MouseLButtonAction* MultipleSelectionState::Instance() {
+	if (instance == 0) {
+		instance = new MultipleSelectionState;
+	}
+	return instance;
+}
+
+void MultipleSelectionState::MouseLButtonUp(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY) {
+
+}
+void MultipleSelectionState::MouseLButtonDown(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY) {
+
+}
+
+void MultipleSelectionState::MouseLButtonDrag(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY, CPaintDC *cPaintDC) {
+
+}
