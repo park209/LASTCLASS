4390a5e9c83175a26d3a36527520c668891dfdef
GooboSeung <ui7878@naver.com>
2017-09-28 11:26:55 +0900

    템플릿 확대 축소 및 라인 임시선 제한 수정끝

--- LastClass/MovingLine.h
+++ LastClass/MovingLine.h
@@ -1,5 +1,8 @@
+//MovingLine.h
+
 #ifndef _MOVINGLINE_H
 #define _MOVINGLINE_H
+
 #include "MouseLButtonAction.h"
 
 
@@ -9,11 +12,10 @@ public:
 
 	virtual void MouseLButtonUp(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY);
 	virtual void MouseLButtonDown(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY);
-	virtual void MouseLButtonDrag(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY, CPaintDC *cPaintDC);
+	virtual void MouseLButtonDrag(MouseLButton *mouseLButton, Diagram *diagram, Selection *selection, Long  startX, Long startY, Long currentX, Long currentY, CPaintDC *cPatinDC);
 protected:
 	//DrawingClass();
 private:
 	static MovingLine* instance;
 };
 #endif // !_MOVINGLINE_H
-
