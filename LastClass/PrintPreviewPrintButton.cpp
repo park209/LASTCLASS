d6f564d48d9e52c83f1d58ea2df8be2abd261ae1
wsx3672 <31624875+wsx3672@users.noreply.github.com>
2017-10-11 20:17:43 +0900

    2017 10 11  Merge

--- LastClass/PrintPreviewPrintButton.cpp
+++ LastClass/PrintPreviewPrintButton.cpp
@@ -13,13 +13,16 @@ PrintPreviewPrintButton::~PrintPreviewPrintButton() {
 }
 
 void PrintPreviewPrintButton::ButtonPress(PrintPreview *printPreview) {
-	CPrintDialog printDialog(FALSE, PD_ALLPAGES | PD_USEDEVMODECOPIES);
+
+	CPrintDialog printDialog(FALSE, PD_ALLPAGES | PD_RETURNDC, printPreview);
 
 	INT_PTR int_ptr = printDialog.DoModal();
 
 
 	if (int_ptr == IDOK) {
 		CDC dc;
+		int count = printDialog.GetCopies();
+		Long i = 1;
 		dc.Attach(printDialog.GetPrinterDC());
 
 		dc.m_bPrinting = TRUE;
@@ -34,23 +37,28 @@ void PrintPreviewPrintButton::ButtonPress(PrintPreview *printPreview) {
 		BOOL bPrintingOK = dc.StartDoc(&di);
 
 		CPrintInfo Info;
+
 		Info.m_rectDraw.SetRect(0, 0, dc.GetDeviceCaps(HORZRES), dc.GetDeviceCaps(VERTRES));
 		Info.SetMaxPage(2);
 		printPreview->OnBeginPrinting(&dc, &Info);
-		for (UINT page = Info.GetMinPage(); page <= Info.GetMaxPage() && bPrintingOK; page++) {
-			dc.StartPage();
-			Info.m_nCurPage = page;
-			printPreview->OnPrint(&dc, &Info, page);
 
-			bPrintingOK = (dc.EndPage() > 0);
+		for (UINT page = Info.GetMinPage(); page <= Info.GetMaxPage() && bPrintingOK; page++) {
+			i = 1;
+			while (i <= count) {
+				dc.StartPage();
+				Info.m_nCurPage = page;
+				printPreview->OnPrint(&dc, &Info, page);
+				bPrintingOK = (dc.EndPage() > 0);
+				i++;
+			}
+
 
 		}
 		printPreview->OnEndPrinting(&dc, &Info);
 		if (bPrintingOK) dc.EndDoc();
 		else dc.AbortDoc();
 
-		//dc.DeleteDC();
 		dc.Detach();
 	}
-	//printDialog.EndDialog(0);
+
 }
