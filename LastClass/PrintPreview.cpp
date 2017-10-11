d6659913c60f7ffbdd308169179c13c509765272
wsx3672 <31624875+wsx3672@users.noreply.github.com>
2017-10-11 20:04:50 +0900

    2017 10 11 프린트 매수설정 가능

--- LastClass/PrintPreview.cpp
+++ LastClass/PrintPreview.cpp
@@ -229,13 +229,15 @@ void PrintPreview::OnEndPrinting(CDC *pDc, CPrintInfo *pInfo) {
 }
 void PrintPreview::OnPrintButton() {
 
-	CPrintDialog printDialog(FALSE, PD_ALLPAGES | PD_USEDEVMODECOPIES,this);
+	CPrintDialog printDialog(FALSE, PD_ALLPAGES | PD_RETURNDC,this);
 
 	INT_PTR int_ptr = printDialog.DoModal();
 
 
 	if (int_ptr == IDOK) {
 		CDC dc;
+		int count =printDialog.GetCopies();
+		Long i = 1;
 		dc.Attach(printDialog.GetPrinterDC());
 		
 		dc.m_bPrinting = TRUE;
@@ -250,15 +252,21 @@ void PrintPreview::OnPrintButton() {
 		BOOL bPrintingOK = dc.StartDoc(&di);
 
 		CPrintInfo Info;
+		
 		Info.m_rectDraw.SetRect(0, 0, dc.GetDeviceCaps(HORZRES),dc.GetDeviceCaps(VERTRES));
 		Info.SetMaxPage(2);
 		OnBeginPrinting(&dc, &Info);
-		for (UINT page = Info.GetMinPage(); page <= Info.GetMaxPage() && bPrintingOK; page++) {
-			dc.StartPage();
-			Info.m_nCurPage = page;
-			this->OnPrint(&dc,&Info,page);
 
-			bPrintingOK = (dc.EndPage() > 0);
+		for (UINT page = Info.GetMinPage(); page <= Info.GetMaxPage() && bPrintingOK  ; page++) {
+			i = 1;
+			while (i <= count) {
+				dc.StartPage();
+				Info.m_nCurPage = page;
+				this->OnPrint(&dc, &Info, page);
+				bPrintingOK = (dc.EndPage() > 0);
+				i++;
+			}
+		
 
 		}
 		this->OnEndPrinting(&dc, &Info);
