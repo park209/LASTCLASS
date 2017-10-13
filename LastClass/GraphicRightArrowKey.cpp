9a445786c613e38f27476fc84adf08cedbd8359c
안 현지 <anji2207@naver.com>
2017-10-13 16:32:29 +0900

    화살표 다됨

--- LastClass/GraphicRightArrowKey.cpp
+++ LastClass/GraphicRightArrowKey.cpp
@@ -4,6 +4,12 @@
 #include "ClassDiagramForm.h"
 #include "TextEdit.h"
 #include "OnHScrollLineRight.h"
+#include "Selection.h"
+#include "Diagram.h"
+#include "SmartPointer.h"
+#include "ArrayIterator.h"
+#include "Class.h"
+#include "Relation.h"
 
 GraphicRightArrowKey::GraphicRightArrowKey() {
 }
@@ -15,8 +21,19 @@ GraphicRightArrowKey::~GraphicRightArrowKey() {
 }
 
 void GraphicRightArrowKey::KeyPress(ClassDiagramForm *classDiagramForm, CDC *cdc) {
-	OnHScrollLineRight onHScrollLineRight;
-	onHScrollLineRight.Scrolling(classDiagramForm);
+	MovingVisitor movingVisitor;
+	if (classDiagramForm->selection->GetLength() == 0) {
+		OnHScrollLineRight onHScrollLineRight;
+		onHScrollLineRight.Scrolling(classDiagramForm);
+	}
+	else if (classDiagramForm->selection->GetLength() == 1) {
+		FigureComposite *object = static_cast<FigureComposite*>(classDiagramForm->selection->GetAt(0));
+		CRect rect{ object->GetX() + 5,object->GetY(), object->GetX() + object->GetWidth() + 5,object->GetY() + object->GetHeight() };
+		BOOL isOverlap = classDiagramForm->diagram->CheckOverlap(rect, object);
+		if (isOverlap == FALSE) {
+			classDiagramForm->selection->Accept(classDiagramForm->diagram, movingVisitor, 1, 0);
+		}
+	}
 }
 
 void GraphicRightArrowKey::KeyPress(TextEdit *textEdit) {
--- LastClass/GraphicUpArrowKey.cpp
+++ LastClass/GraphicUpArrowKey.cpp
@@ -4,6 +4,13 @@
 #include "ClassDiagramForm.h"
 #include "TextEdit.h"
 #include "OnVScrollLineUp.h"
+#include "Selection.h"
+#include "Diagram.h"
+#include "SmartPointer.h"
+#include "ArrayIterator.h"
+#include "Class.h"
+#include "Relation.h"
+
 GraphicUpArrowKey::GraphicUpArrowKey() {
 }
 
@@ -14,9 +21,21 @@ GraphicUpArrowKey::~GraphicUpArrowKey() {
 }
 
 void GraphicUpArrowKey::KeyPress(ClassDiagramForm *classDiagramForm, CDC *cdc) {
-	OnVScrollLineUp onVScrollLineUp;
-	onVScrollLineUp.Scrolling(classDiagramForm);
+	MovingVisitor movingVisitor;
+	if (classDiagramForm->selection->GetLength() == 0) {
+		OnVScrollLineUp onVScrollLineUp;
+		onVScrollLineUp.Scrolling(classDiagramForm);
+	}
+	else if (classDiagramForm->selection->GetLength() == 1) {
+		FigureComposite *object = static_cast<FigureComposite*>(classDiagramForm->selection->GetAt(0));
+		CRect rect{ object->GetX() ,object->GetY() - 5, object->GetX() + object->GetWidth(),object->GetY() + object->GetHeight() - 5 };
+		BOOL isOverlap = classDiagramForm->diagram->CheckOverlap(rect, object);
+		if (isOverlap == FALSE) {
+			classDiagramForm->selection->Accept(classDiagramForm->diagram, movingVisitor, 0, -1);
+		}
+	}
 }
+
 
 void GraphicUpArrowKey::KeyPress(TextEdit *textEdit) {
 }
