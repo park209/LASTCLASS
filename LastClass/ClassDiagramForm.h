bab50c7655b8e517979f6e06868663453982068d
Hyungjin <thesockerr@naver.com>
2017-10-13 11:51:50 +0900

    before merge

--- LastClass/ClassDiagramForm.h
+++ LastClass/ClassDiagramForm.h
@@ -63,6 +63,7 @@ protected:
 	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar); 
 	afx_msg BOOL OnMouseWheel(UINT nFlags,	short zDelta,CPoint pt);
+	afx_msg void OnNcMouseMove(UINT nHitTest, CPoint point);
 	DECLARE_MESSAGE_MAP()
 };
 
