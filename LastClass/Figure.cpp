7d68ffb0c60a3c16fff2d1e5272e13d9d72e53ae
Hyungjin <thesockerr@naver.com>
2017-10-17 11:33:20 +0900

    before merge

--- LastClass/Figure.cpp
+++ LastClass/Figure.cpp
@@ -3,40 +3,47 @@
 #include "Figure.h"
 #include "Relation.h"
 
+#include "LastClass.h"
+#include "ClassDiagramForm.h"
+
 Figure::Figure() {
+	LastClass *test = (LastClass*)(CFrameWnd::FindWindow(NULL, "lastClass"));
 	this->x = 0;
 	this->y = 0;
 	this->width = 0;
 	this->height = 0;
 	this->content = "";
-	this->minimumWidth = 120;
+	this->minimumWidth = 120 * test->classDiagramForm->zoomRate / 100;
 	this->minimumHeight = 29;
 	this->fontSize = 25;
 }
 
 Figure::Figure(Long x, Long y, Long width, Long height) {
+	LastClass *test = (LastClass*)(CFrameWnd::FindWindow(NULL, "lastClass"));
 	this->x = x;
 	this->y = y;
 	this->width = width;
 	this->height = height;
 	this->content = "";
-	this->minimumWidth = 120;
+	this->minimumWidth = 120 * test->classDiagramForm->zoomRate / 100;
 	this->minimumHeight = 29;
 	this->fontSize = 25;
 }
 
 Figure::Figure(Long x, Long y, Long width, Long height, string content) {
+	LastClass *test = (LastClass*)(CFrameWnd::FindWindow(NULL, "lastClass"));
 	this->x = x;
 	this->y = y;
 	this->width = width;
 	this->height = height;
 	this->content = content;
-	this->minimumWidth = 120; //여기도
+	this->minimumWidth = 120 * test->classDiagramForm->zoomRate / 100; //여기도
 	this->minimumHeight = 29; // 입력받을 문자열에 맞게 값 줘야하는데
 	this->fontSize = 25;
 }
 
 Figure::Figure(Long x, Long y, Long width, Long height, Long minimumWidth, Long minimumHeight, string content) {
+	LastClass *test = (LastClass*)(CFrameWnd::FindWindow(NULL, "lastClass"));
 	this->x = x;
 	this->y = y;
 	this->width = width;
