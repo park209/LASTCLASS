81d74e6026d96431393a60e3e0934b89251d4c1b
wsx3672 <31624875+wsx3672@users.noreply.github.com>
2017-10-23 12:32:10 +0900

    2017 10 23 Figure에 EndPointFigure 추가 클래스중첩가능하도록

--- LastClass/DrawingRealization.cpp
+++ LastClass/DrawingRealization.cpp
@@ -67,6 +67,7 @@ void DrawingRealization::MouseLButtonUp(MouseLButton *mouseLButton, ClassDiagram
 		Realization object(cross1.x, cross1.y, cross2.x - cross1.x, cross2.y - cross1.y);
 		index = static_cast<FigureComposite*>(selection->GetAt(0))->Add(object.Clone());
 		figure = static_cast<FigureComposite*>(selection->GetAt(0))->GetAt(index);
+		figure->SetEndPointFigure(classObject2);
 	}
 	
 	selection->DeleteAllItems();
