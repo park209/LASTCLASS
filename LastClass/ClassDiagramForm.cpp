3b228f1b2df09dcb4bcab46b2377b3c6d9cfbd8b
Arc <teats@naver.com>
2017-10-23 10:22:53 +0900

    correct line minimumwidth

--- LastClass/ClassDiagramForm.cpp
+++ LastClass/ClassDiagramForm.cpp
@@ -1013,7 +1013,7 @@ BOOL ClassDiagramForm::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt) {
 		}
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////
 		KnockKnock *knocking = new KnockKnock;
-		knocking->Knocking(this);
+		//knocking->Knocking(this);
 		if (knocking != NULL) {
 			delete knocking;
 		}
