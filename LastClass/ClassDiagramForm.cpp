35e8fea116b52c6b57c15a4445d281345a1a0677
wsx3672 <31624875+wsx3672@users.noreply.github.com>
2017-11-06 20:09:31 +0900

    2017 11 06 최종본 (ZoomRate값만 변경하면 됨)

--- LastClass/ClassDiagramForm.cpp
+++ LastClass/ClassDiagramForm.cpp
@@ -1139,8 +1139,8 @@ BOOL ClassDiagramForm::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt) {
 		this->preZoom = this->zoomRate;
 		if (zDelta <= 0) { //마우스 휠 다운
 			this->zoomRate -= 10;
-			if (this->zoomRate < 10) {
-				this->zoomRate = 10;
+			if (this->zoomRate < 50) {
+				this->zoomRate = 50;
 			}
 		}
 		else {  //마우스 휠 업
